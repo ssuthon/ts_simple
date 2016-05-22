bb03cbefa7942d57632afd5a8f2aa6dd9a07b37a
Suthon <suthon@gmail.com>
2016-05-22 20:22:46 +0700

    init

--- build/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var _ = require('underscore');
-var d = { key: 'abc', value: '10' };
-console.log(d.value);
-var s = [2, 3];
-var total = _.reduce(s, function (p, c) { return p + c; }, 0);
-console.log(total);
